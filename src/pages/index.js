807549d2a33b9bb9c34dbc221d1cc69a7cfdff18
Sumiya Ushiro <u.sumi0820@gmail.com>
2020-02-29 17:43:58 +0800

    Update landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import LandingBio from "../components/landing-bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import { css } from "@emotion/core"
 import styled from "@emotion/styled"
 
 
