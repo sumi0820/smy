3a659974bbcfd6bc593aef59dfb061e312f572d7
Sumiya Ushiro <u.sumi0820@gmail.com>
2020-02-29 17:37:04 +0800

    Update landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,26 @@ import LandingBio from "../components/landing-bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import styled from "@emotion/styled"
 
+
+const MarkerHeader = styled.h2`
+  display: inline;
+  border-radius: 1em 0 1em 0;
+  background-image: linear-gradient(
+    -100deg,
+    rgba(255, 250, 150, 0.15),
+    #057989 -50%,
+    rgba(255, 250, 150, 0.25)
+  );
+`
 
 
 const IndexPage = () => (
   <Layout>
+  <MarkerHeader className="marker-header">
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+   </MarkerHeader>
     <LandingBio />
   </Layout>
 )
